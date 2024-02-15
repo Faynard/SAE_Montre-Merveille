b716405eac12f55d0c64d950f7701b9cc3ae81f5
MBausson <maximilienbausson@icloud.com>
2024-02-15 08:10:43 +0100

    Amélioration page d'accueil

--- montres-et-merveilles/resources/views/acceuil.blade.php
+++ montres-et-merveilles/resources/views/acceuil.blade.php
@@ -1,7 +1,31 @@
 @extends('layouts.default')
 
 @section('content')
-<video autoplay muted loop>
-    <source src="{{ asset('videos/accueil.mp4') }}" />
-</video>
+<div class="relative bg-black">
+    <div class="relative h-[calc(100vh+113px)]">
+        <video class="absolute object-cover h-screen w-screen" autoplay muted loop>
+            <source src="{{ asset('videos/accueil.mp4') }}" />
+        </video>
+
+        <div class="absolute flex flex-col gap-8 lg:top-[30%] ml-16 text-white text-balance">
+            <p class="flex flex-col text-5xl uppercase max-w-96">
+                <span class="font-['Cinzel'] font-thin">Une Royal OAK Concept</span>
+                <span class="font-['NotoSerif'] italic">À la féminité affirmée</span>
+            </p>
+
+            <p class="w-2/5">
+                Montres & Mervéilles dévoile une nouvelle édition limitée de la Royal Oak Concept Tourbillon Volant,
+                imaginée
+                en collaboration avec la créatrice de Haute Couture Tamara Ralph.
+            </p>
+
+            <p class="flex gap-2 items-center">
+                <span class="block w-12 border border-white"></span>
+                <span>En savoir plus</span>
+            </p>
+        </div>
+    </div>
+
+    <div class="h-screen"></div>
+</div>
 @endsection
--- montres-et-merveilles/resources/views/layouts/default.blade.php
+++ montres-et-merveilles/resources/views/layouts/default.blade.php
@@ -3,19 +3,16 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Montres & merveilles</title>
 
     <link rel="stylesheet" href="css/app.css">
 
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="antialiased">
+<body>
     <nav class="flex items-center justify-center gap-16 font-['Comfortaa'] uppercase h-28">
 
         <div class="flex gap-5">
