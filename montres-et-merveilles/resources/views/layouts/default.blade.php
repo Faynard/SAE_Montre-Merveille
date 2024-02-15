a0b88659028b30b8e9860145e198701be4bc2b88
Mathieu LE BRAS <mathieu.le-bras@etu.univ-nantes.fr>
2024-02-15 20:24:22 +0000

    Resolve "faire le front"

--- montres-et-merveilles/resources/views/layouts/default.blade.php
+++ montres-et-merveilles/resources/views/layouts/default.blade.php
@@ -8,33 +8,46 @@
     <title>Montres & merveilles</title>
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/accueil.css') }}">
 
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="{{ asset('js/navbar.js') }}" defer></script>
 </head>
 
 <body>
-    <nav class="flex items-center justify-center gap-16 font-['Comfortaa'] uppercase h-28">
+    <header>
+        <nav id="navbar"
+            class="navbar flex items-center justify-center gap-16 font-['Comfortaa'] uppercase h-28">
 
-        <div class="flex gap-5">
-            <a href="{{ route('product.index') }}">Nos montres</a>
-            <a href="{{ route('contact.index') }}">Contact</a>
-        </div>
+            <div class="flex gap-5">
+                <a href="{{ route('product.index') }}">Nos montres</a>
+                <a href="{{ route('contact.index') }}">Contact</a>
+            </div>
 
-        <a href="{{ route('acceuil.index') }}" class="flex flex-col gap-1">
-            <img src="{{ asset('images/logo.svg') }}" />
-            <img src="{{ asset('images/swiss-made.svg') }}" class="h-6" />
-        </a>
+            <a href="{{ route('acceuil.index') }}" class="flex flex-col gap-1">
+                <img id="logo" data-alt-src="{{ asset('images/logo-white.svg') }}"
+                    src="{{ asset('images/logo.svg') }}" />
+                <img id="swiss-logo" data-alt-src="{{ asset('images/swiss-made-white.svg') }}"
+                    src="{{ asset('images/swiss-made.svg') }}" class="h-6" />
+            </a>
 
-        <div class="flex gap-5">
-            <a href="{{ route('user.profile') }}">Mon compte</a>
-            <a href="{{ route('boutiques.index') }}">Boutiques</a>
-        </div>
+            <div class="flex gap-5">
+                <a href="{{ route('user.profile') }}">Mon compte</a>
+                <a href="{{ route('boutiques.index') }}">Boutiques</a>
+            </div>
 
-    </nav>
+        </nav>
+    </header>
 
-    <main>
+    <main class="min-h-screen">
         @yield('content')
     </main>
+
+    <footer class="bg-green-900">
+        <div class="flex items-center justify-center h-96">
+            <span>footer</span>
+        </div>
+    </footer>
 </body>
 
 </html>
