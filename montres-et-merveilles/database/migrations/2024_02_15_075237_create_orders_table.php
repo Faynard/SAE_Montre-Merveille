c44cb2dbfff7498c0dad7757cc171406e98dfbb9
Mathieu LE BRAS <mathieu.le-bras@etu.univ-nantes.fr>
2024-02-19 10:14:35 +0000

    Resolve "ajouter la suppression en cascade sur les modeles"

--- montres-et-merveilles/database/migrations/2024_02_15_075237_create_orders_table.php
+++ montres-et-merveilles/database/migrations/2024_02_15_075237_create_orders_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(\App\Models\User::class)->constrained();
+            $table->foreignIdFor(\App\Models\User::class)->constrained()->cascadeOnDelete();
             $table->float('price');
             $table->timestamps();
         });
