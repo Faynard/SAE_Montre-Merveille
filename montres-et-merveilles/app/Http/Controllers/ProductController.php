ff6017166fcdec0d8c9124908bf9235657758c44
MBausson <maximilienbausson@icloud.com>
2024-02-21 21:56:05 +0100

    Présentation bouton rechercher, refactorisation

--- montres-et-merveilles/app/Http/Controllers/ProductController.php
+++ montres-et-merveilles/app/Http/Controllers/ProductController.php
@@ -4,55 +4,60 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Query\Builder;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index(Request $request)
     {
+        //  Indique les valeurs par défaut des filtres
+        $request->mergeIfMissing(["name" => '', "size" => 0, "movement" => null, "material" => null]);
+        $filters = $request->only(['name', 'size', 'movement', 'material']);
+
         //  Au lieu de récupérer tout les produits et de les trier après,
         //      on ajoute des clauses WHERE directement dans le code SQL de la requête
         //      si les filtres sont appliqués
         //  products_filtered effectue ceci, et retourne les éléments filtrés
-        $products_query = $this->products_filtered(DB::table("products"), [
-            "name" => $request->input("name"),
-            "size" => $request->input("size"),
-            "movement" => $request->input("movement"),
-            "material" => $request->input("material"),
-        ]);
+        $products_query = DB::table('products');
+
+        //  Ajoute des clauses WHERE pour chaque filtre s'ils ont une valeur
+        foreach ($filters as $field => $value) {
+            if ($value) {
+                switch ($field) {
+                    case 'name':
+                        //  Permet de faire une recherche par nom insensible à la casse
+                        //  Nous aurions pu aussi utiliser ILIKE
+                        //      $products_query->where('name', 'ilike', '%' . $value . '%');
+                        //  Mais cet opérateur n'est pas supporté sur SQLITE, nos tests auraient donc échoués à cause de cela
+                        $products_query->where(DB::raw('lower(name)'), 'like', '%' . strtolower($value) . '%');
+                        break;
+                    case 'size':
+                        $products_query->where('size', '<=', $value);
+                        break;
+                    case 'movement':
+                    case 'material':
+                        $products_query->where($field, $value);
+                        break;
+                }
+            }
+        }
 
         // nombre d'éléments par page
         $page_length = 9;
+        $page = $request->input('page', 1);
 
-        $page_index = 1;
-
-        if (array_key_exists('page', $request->all())) {
-
-            $page_input = $request->input('page');
-            if (!is_numeric($page_input) || $page_input < 1) {
-                abort(400);
-            }
-
-            $page_index = $request->input('page');
+        if (!is_numeric($page) || $page < 1) {
+            abort(400);
         }
 
-        $products = $products_query->offset(($page_index - 1) * $page_length)->limit($page_length)->get();
+        $products = $products_query->offset(($page - 1) * $page_length)->limit($page_length)->get();
 
         // recuperer le nombre de produits dans la base de données pour savoir combien de pages on doit afficher
         $products_count = Product::count();
-        $pages_count = (int) ceil($products_count / $page_length);
+        $nb_pages = (int) ceil($products_count / $page_length);
 
-        return view("product.catalog", [
-            "page" => $page_index,
-            "nb_pages" => $pages_count,
-            "products_count" => $products_count,
-            "products" => $products,
-            "name" => $request->input("name"),
-            "size" => $request->input("size") ?? 0,
-            "movement" => $request->input("movement") ?? "",
-            "material" => $request->input("material") ?? "",
-        ]);
+        //  Retourne la vue avec les données de page, produits et de filtres
+        return view("product.catalog", array_merge(compact('page', 'nb_pages', 'products_count', 'products'), $filters));
     }
 
     //  Récupère le produit via l'ID renseigné en paramètre afin de le renseigner dans la vue product
@@ -81,27 +86,5 @@ class ProductController extends Controller
         $product->delete();
 
         return redirect()->route("product.index")->with(["hasNotification" => true, "notificationTitle" => "Suppression", "notificationContent" => "Le produit a bien été supprimé"]);
-    }
-
-    //  Modifie la requête SQL permettant de récupérer tous les produits, en ajouter une clause WHERE par filtre renseigné
-    private function products_filtered(Builder $products_query, array $filters)
-    {
-        if (array_key_exists('name', $filters) && $filters['name']) {
-            $products_query = $products_query->where('name', 'LIKE', '%' . $filters['name'] . '%');
-        }
-
-        if (array_key_exists('size', $filters) && $filters['size']) {
-            $products_query = $products_query->where('size', '<=', $filters['size']);
-        }
-
-        if (array_key_exists('movement', $filters) && $filters['movement']) {
-            $products_query = $products_query->where('movement', $filters['movement']);
-        }
-
-        if (array_key_exists('material', $filters) && $filters['material']) {
-            $products_query = $products_query->where('material', $filters['material']);
-        }
-
-        return $products_query;
     }
 }
--- montres-et-merveilles/resources/views/product/catalog.blade.php
+++ montres-et-merveilles/resources/views/product/catalog.blade.php
@@ -1,127 +1,129 @@
-@extends("layouts.default")
+@extends('layouts.default')
 
-@section("content")
-<div class="px-12 md:px-26 lg:px-44 mt-20 mb-12">
-    <!-- Barre de recherche -->
-    <form class="flex flex-col gap-1" action="{{ route('product.index') }}" method="GET">
-        <div class="relative">
-            <input
-                class="border-b-2 border-gray-400 focus:border-gray-500 py-2 text-2xl w-full md:text-4xl font-thin focus:outline-none pl-4 transition"
-                name="name" placeholder="Rechercher des montres" value="{{ $name }}" />
-            <button type="submit" class="absolute inset-y-0 right-0">
-                <img class="mb-6" src="{{ asset('images/search-icon.svg') }}" />
-            </button>
-        </div>
-    </form>
+@section('content')
+    <div class="px-12 md:px-26 lg:px-44 mt-20 mb-12">
+        <!-- Barre de recherche -->
+        <form class="flex flex-col gap-1" action="{{ route('product.index') }}" method="GET">
+            <div class="relative">
+                <input
+                    class="border-b-2 border-gray-400 focus:border-gray-500 py-2 text-2xl w-full md:text-4xl font-thin focus:outline-none pl-4 transition"
+                    name="name" placeholder="Rechercher des montres" value="{{ $name }}" />
 
-    <!-- Filtres -->
-    <div class="mt-32 border-b border-gray-400 flex gap-8 py-4">
-        <div>
-            <span class="text-gray-400 font-thin">Filtrer par</span>
+                <button type="submit" class="absolute inset-y-0 right-0">
+                    <img class="mb-6" src="{{ asset('images/search-icon.svg') }}" />
+                </button>
+            </div>
+
+
+            <!-- Filtres -->
+            <div class="mt-32 border-b border-gray-400 flex gap-8 py-4">
+                <div>
+                    <span class="text-gray-400 font-thin">Filtrer par</span>
+                </div>
+                <div>
+                    <span id="filter-size-title" class="cursor-pointer transition hover:text-gray-500">Taille</span>
+                </div>
+                <div>
+                    <span id="filter-movement-title" class="cursor-pointer transition hover:text-gray-500">Mouvement</span>
+                </div>
+                <div>
+                    <span id="filter-material-title" class="cursor-pointer transition hover:text-gray-500">Matériaux</span>
+                </div>
+
+                <div class="ml-auto">
+                    <button class="font-semibold underline underline-offset-4">Rechercher</button>
+                </div>
+            </div>
+
+            <div>
+                <div id="filter-size-container" class="mt-8 hidden flex gap-8">
+                    <label class="font-medium">Taille maximale (mm.)</label>
+                    <div class="flex gap-2">
+                        <input class="border-b-2 border-gray-400 font-thin text-lg focus:outline-none" type="range"
+                            name="size" value="{{ $size }}" min="0" max="60"
+                            oninput="this.nextElementSibling.value = this.value" />
+                        <output>{{ $size }}</output>
+                    </div>
+                </div>
+
+                <div id="filter-movement-container" class="mt-8 hidden flex gap-8">
+                    <label class="font-medium">Type de mouvement</label>
+                    <div class="flex gap-2">
+                        <select name="movement">
+                            <option selected disabled>--- Mouvement ---</option>
+
+                            @foreach (App\Models\Product::$movements as $optMovement)
+                                <option value="{{ $optMovement }}" {{ $optMovement == $movement ? 'selected' : '' }}>
+                                    {{ $optMovement }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+
+                <div id="filter-material-container" class="mt-8 hidden flex gap-8">
+                    <label class="font-medium">Matériaux</label>
+                    <div class="flex gap-2">
+                        <select name="material">
+                            <option selected disabled>--- Matériaux ---</option>
+
+                            @foreach (App\Models\Product::$materials as $optMaterial)
+                                <option value="{{ $optMaterial }}" {{ $optMaterial == $material ? 'selected' : '' }}>
+                                    {{ $optMaterial }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+        </form>
+
+        <div class="flex items-center place-content-between">
+            {{-- nb élément de la requête --}}
+            <p class="my-8 text-gray-400 font-thin">{{ $products_count }} montres</p>
+
+            {{-- pagination --}}
+            <div class="flex items-center gap-1">
+                @for ($i = 1; $i <= $nb_pages; $i++)
+                    @if ($i == $page)
+                        {{-- index de la page actuel --}} <span class="text-lg font-bold">{{ $i }}</span>
+                    @else
+                        <a href="{{ route('product.index', ['page' => $i]) }}"
+                            class="text-base font-thin">{{ $i }}</a>
+                    @endif
+                @endfor
+            </div>
         </div>
-        <div>
-            <span id="filter-size-title" class="cursor-pointer transition hover:text-gray-500">Taille</span>
-        </div>
-        <div>
-            <span id="filter-movement-title" class="cursor-pointer transition hover:text-gray-500">Mouvement</span>
-        </div>
-        <div>
-            <span id="filter-material-title" class="cursor-pointer transition hover:text-gray-500">Matériaux</span>
+
+        <div class="grid grid-cols-3 gap-4">
+            @foreach ($products as $product)
+                <article class="bg-neutral-200 p-6 flex flex-col items-center gap-6 group">
+                    <a href="{{ route('product.show', ['product' => $product->id]) }}" class="text-lg font-bold"><img
+                            class="w-40 drop-shadow-lg transition ease-in group-hover:scale-105 duration-500"
+                            src="{{ asset('images/montre_1.png') }}" /></a>
+
+                    <p class="flex flex-col items-center gap-2">
+                        <a href="{{ route('product.show', ['product' => $product->id]) }}"
+                            class="text-lg text-balance text-center font-bold">{{ $product->name }}</a>
+                        <span class="font-thin">Ultra-Complication Universelle</span>
+                    </p>
+
+                    <span class="text-sm font-light text-gray-500">
+                        {{ $product->movement }}, {{ $product->size }}mm, {{ $product->material }}
+                    </span>
+                </article>
+            @endforeach
         </div>
     </div>
 
-    <form>
-        <div id="filter-size-container" class="mt-8 hidden flex gap-8">
-            <label class="font-medium">Taille maximale (mm.)</label>
-            <div class="flex gap-2">
-                <input class="border-b-2 border-gray-400 font-thin text-lg focus:outline-none" type="range" name="size"
-                    value="{{ $size }}" min="0" max="60" oninput="this.nextElementSibling.value = this.value" />
-                <output>{{ $size }}</ouput>
-            </div>
-            <button class="underline underline-offset-4">Rechercher</button>
-        </div>
+    <script>
+        const filtersName = ["size", "movement", "material"];
 
-        <div id="filter-movement-container" class="mt-8 hidden flex gap-8">
-            <label class="font-medium">Type de mouvement</label>
-            <div class="flex gap-2">
-                <select name="movement">
-                    <option selected disabled>--- Mouvement ---</option>
+        filtersName.forEach(filter => {
+            const filterTitle = document.getElementById(`filter-${filter}-title`);
+            const filterContainer = document.getElementById(`filter-${filter}-container`);
 
-                    @foreach (App\Models\Product::$movements as $movement)
-                    <option value="{{ $movement }}">{{ $movement }}</option>
-                    @endforeach
-                </select>
-            </div>
-            <button class="underline underline-offset-4">Rechercher</button>
-        </div>
-
-        <div id="filter-material-container" class="mt-8 hidden flex gap-8">
-            <label class="font-medium">Matériaux</label>
-            <div class="flex gap-2">
-                <select name="material">
-                    <option selected disabled>--- Matériaux ---</option>
-
-                    @foreach (App\Models\Product::$materials as $material)
-                    <option value="{{ $material }}">{{ $material }}</option>
-                    @endforeach
-                </select>
-            </div>
-
-            <button class="underline underline-offset-4">Rechercher</button>
-        </div>
-    </form>
-
-    <div class="flex items-center place-content-between">
-        {{-- nb élément de la requête --}}
-        <p class="my-8 text-gray-400 font-thin">{{ $products_count }} montres</p>
-
-        {{-- pagination --}}
-        <div class="flex items-center gap-1">
-            @for ($i = 1; $i <= $nb_pages; $i++)
-                @if ($i == $page) 
-                    {{-- index de la page actuel --}}
-                    <span class="text-lg font-bold">{{ $i }}</span>
-                @else
-                    <a href="{{ route('product.index', ['page' => $i]) }}" class="text-base font-thin">{{ $i }}</a>
-                @endif
-            @endfor
-        </div>
-    </div>
-
-    <div class="grid grid-cols-3 gap-4">
-        @foreach ($products as $product)
-        <article class="bg-neutral-200 p-6 flex flex-col items-center gap-6 group">
-            <a href="{{ route('product.show', ['product' => $product->id]) }}" class="text-lg font-bold"><img
-                    class="w-40 drop-shadow-lg transition ease-in group-hover:scale-105 duration-500"
-                    src="{{ asset('images/montre_1.png') }}" /></a>
-
-            <p class="flex flex-col items-center gap-2">
-                <a href="{{ route('product.show', ['product' => $product->id]) }}"
-                    class="text-lg text-balance text-center font-bold">{{
-                    $product->name }}</a>
-                <span class="font-thin">Ultra-Complication Universelle</span>
-            </p>
-
-            <span class="text-sm font-light text-gray-500">
-                {{ $product->movement }}, {{ $product->size }}mm, {{ $product->material }}
-            </span>
-        </article>
-        @endforeach
-    </div>
-</div>
-
-<script>
-    const filtersName = ["size", "movement", "material"];
-
-    filtersName.forEach(filter =>
-    {
-        const filterTitle = document.getElementById(`filter-${filter}-title`);
-        const filterContainer = document.getElementById(`filter-${filter}-container`);
-
-        filterTitle.addEventListener('click', function (e)
-        {
-            filterContainer.classList.toggle("hidden");
+            filterTitle.addEventListener('click', function(e) {
+                filterContainer.classList.toggle("hidden");
+            });
         });
-    });
-</script>
+    </script>
 @endsection
